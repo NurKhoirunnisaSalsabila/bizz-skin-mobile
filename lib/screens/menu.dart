56a46c9416855eef878c914241d777db45b4649c
NurKhoirunnisaSalsabila <nur.khoirunnisa@ui.ac.id>
2024-11-12 19:11:57 +0700

    tugas8 done yh kak

--- lib/screens/menu.dart
+++ lib/screens/menu.dart
@@ -1,17 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:bizz_skin_mobile/widgets/left_drawer.dart';
+import 'package:bizz_skin_mobile/widgets/product_card.dart';
 
-class MyHomePage extends StatelessWidget {
-  final String npm = '2306165534'; // NPM
-  final String name = 'Nur Khoirunnisa Salsabila'; // Nama
-  final String className = 'PBP A'; // Kelas
+class MenuScreen extends StatelessWidget {
+  final String userId = '2306165534';
+  final String userName = 'Nur Khoirunnisa Salsabila';
+  final String className = 'PBP A';
+  final List<ItemHomepage> items;
 
-  MyHomePage({super.key});
-
-  final List<ItemHomepage> items = [
-    ItemHomepage("Lihat Daftar Produk", Icons.production_quantity_limits_rounded, Colors.pink), // Ungu
-    ItemHomepage("Tambah Produk", Icons.add, Colors.purple), // Ungu
-    ItemHomepage("Logout", Icons.logout, Colors.blue), // Ungu
-  ];
+  MenuScreen({super.key}) 
+      : items = [
+          ItemHomepage("Lihat Daftar Produk", Icons.production_quantity_limits_rounded, const Color.fromARGB(255, 242, 158, 158)), // Dark red
+          ItemHomepage("Tambah Produk", Icons.add, const Color.fromARGB(255, 172, 156, 236)), // Burgundy
+          ItemHomepage("Logout", Icons.logout, const Color(0xFF8B4513)), // Saddle brown
+        ];
 
   @override
   Widget build(BuildContext context) {
@@ -25,7 +27,9 @@ class MyHomePage extends StatelessWidget {
           ),
         ),
         backgroundColor: Theme.of(context).colorScheme.primary,
+        iconTheme: const IconThemeData(color: Colors.white),
       ),
+      drawer: const LeftDrawer(),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
@@ -34,14 +38,12 @@ class MyHomePage extends StatelessWidget {
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                InfoCard(title: 'NPM', content: npm),
-                InfoCard(title: 'Name', content: name),
+                InfoCard(title: 'NPM', content: userId),
+                InfoCard(title: 'Username', content: userName),
                 InfoCard(title: 'Class', content: className),
               ],
             ),
-
             const SizedBox(height: 16.0),
-
             Center(
               child: Column(
                 children: [
@@ -55,7 +57,6 @@ class MyHomePage extends StatelessWidget {
                       ),
                     ),
                   ),
-
                   GridView.count(
                     primary: true,
                     padding: const EdgeInsets.all(20),
@@ -81,7 +82,11 @@ class InfoCard extends StatelessWidget {
   final String title;
   final String content;
 
-  const InfoCard({super.key, required this.title, required this.content});
+  const InfoCard({
+    super.key,
+    required this.title,
+    required this.content,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -103,55 +108,4 @@ class InfoCard extends StatelessWidget {
       ),
     );
   }
-}
-
-class ItemCard extends StatelessWidget {
-  final ItemHomepage item;
-
-  const ItemCard(this.item, {super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Material(
-      color: item.color,
-      borderRadius: BorderRadius.circular(12),
-      child: InkWell(
-        onTap: () {
-          ScaffoldMessenger.of(context)
-            ..hideCurrentSnackBar()
-            ..showSnackBar(SnackBar(
-                content: Text("Kamu telah menekan tombol ${item.name}!")));
-        },
-        child: Container(
-          padding: const EdgeInsets.all(8),
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(
-                  item.icon,
-                  color: Colors.white,
-                  size: 30.0,
-                ),
-                const Padding(padding: EdgeInsets.all(3)),
-                Text(
-                  item.name,
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(color: Colors.white),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class ItemHomepage {
-  final String name;
-  final IconData icon;
-  final Color color;
-
-  ItemHomepage(this.name, this.icon, this.color);
 }
